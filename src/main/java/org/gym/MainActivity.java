6e9e0cc5e87f11cdc4d29c6201d5bf761c997268
overriderb <overriderb@gmail.com>
2014-06-21 02:21:15 +0300

    Created maven project. Added simple activity and configuration.

---
+++ src/main/java/org/gym/MainActivity.java
@@ -0,0 +1,22 @@
+package org.gym;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Gym 3000!");
+    }
+
+}
