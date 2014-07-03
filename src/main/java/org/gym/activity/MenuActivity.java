bed2f47db6a84c3e673ed5da6756e044090fc0df
AndreyNick <askneket@gmail.com>
2014-07-03 19:11:04 +0300

    Added call of ProgramActivity by button clicking

--- src/main/java/org/gym/activity/MenuActivity.java
+++ src/main/java/org/gym/activity/MenuActivity.java
@@ -1,7 +1,9 @@
 package org.gym.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import org.gym.activity.R;
 
 /**
@@ -18,6 +20,11 @@ public class MenuActivity extends Activity {
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    public void startActivity(View view){
+        Intent intent = new Intent(this, ProgramActivity.class);
+        startActivity(intent);
     }
 
 }
