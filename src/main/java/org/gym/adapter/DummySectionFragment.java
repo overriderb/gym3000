be50db4425a5c21b4692e0e440de4c70a881f5b4
overriderb <overriderb@gmail.com>
2014-07-13 12:49:48 +0300

    Revert to commit f59ba1c9c7046d08c8f4331881696da9b50d1b26.

--- src/main/java/org/gym/adapter/DummySectionFragment.java
+++ src/main/java/org/gym/adapter/DummySectionFragment.java
@@ -10,9 +10,6 @@ import org.gym.Workout;
 import org.gym.WorkoutFactory;
 import org.gym.activity.R;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * A dummy fragment representing a section of the app, but that simply
  * displays dummy text.
@@ -29,8 +26,7 @@ public class DummySectionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Workout workoutItem = (Workout) WorkoutFactory.getExercisesCollection(getExerciseTitles(), getExerciseTexts())
-                .get(getArguments().getInt(ARG_SECTION_NUMBER) - 1);
+        Workout workoutItem = (Workout) WorkoutFactory.getExercisesCollection().get(getArguments().getInt(ARG_SECTION_NUMBER) - 1);
 
         View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
         TextView workoutNameTextView = (TextView) rootView.findViewById(R.id.workout_title);
@@ -39,13 +35,5 @@ public class DummySectionFragment extends Fragment {
         workoutNameTextView.setText(workoutItem.getName());
         workoutDescrTextView.setText(workoutItem.getDescription());
         return rootView;
-    }
-
-    private List<String> getExerciseTitles() {
-        return Arrays.asList(getResources().getStringArray(R.array.exerciseTitles));
-    }
-
-    private List<String> getExerciseTexts() {
-        return Arrays.asList(getResources().getStringArray(R.array.exerciseTexts));
     }
 }
