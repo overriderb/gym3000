b4c09cb4d7216df7b2a6623b3c64de5310369e43
AndreyNick <askneket@gmail.com>
2014-10-03 14:59:07 +0300

    Added menu item on history view 'Workout'

--- src/main/java/org/gym/activity/HistoryActivity.java
+++ src/main/java/org/gym/activity/HistoryActivity.java
@@ -1,11 +1,13 @@
 package org.gym.activity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import org.gym.adapter.HistoryPagerAdapter;
-import org.gym.adapter.ProgramPagerAdapter;
 
 /**
  * Created by anni0913 on 08.07.2014.
@@ -28,4 +30,28 @@ public class HistoryActivity extends FragmentActivity {
     public void onStart() {
         super.onStart();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){       //TODO need to discuss what options do we need in options menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.history_layout_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_workout:
+                startProgram();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void startProgram(){
+        Intent intent = new Intent(this, ProgramActivity.class);
+        startActivity(intent);
+    }
+
 }
