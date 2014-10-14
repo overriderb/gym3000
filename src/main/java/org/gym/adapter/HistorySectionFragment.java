49d29522f4a4e4aee23520949449a14851ae84af
AndreyNick <askneket@gmail.com>
2014-10-14 18:30:47 +0300

    Added test launch of  history_pages listView

--- src/main/java/org/gym/adapter/HistorySectionFragment.java
+++ src/main/java/org/gym/adapter/HistorySectionFragment.java
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import org.gym.Workout;
 import org.gym.WorkoutFactory;
 import org.gym.activity.R;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A dummy fragment representing a section of the app, but that simply
@@ -24,7 +28,8 @@ public class HistorySectionFragment extends Fragment {
     Workout workoutItem;
     View rootView;
     TextView workoutNameTextView;
-    //ListView workoutHistoryView;
+    ListView workoutHistoryView;
+    List<String> listOfWorkoutResult = new LinkedList<String>();
 
 
 
@@ -33,12 +38,22 @@ public class HistorySectionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        listOfWorkoutResult.add("result1");
+        listOfWorkoutResult.add("result2");
+        listOfWorkoutResult.add("result3");
+        listOfWorkoutResult.add("result4");
+
+
+        ArrayAdapter<String> historyWorkoutAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.history_list_item_layout, R.id.workout_item_history_text, listOfWorkoutResult);
+
         workoutItem = (Workout) WorkoutFactory.getExercisesCollection().get(getArguments().getInt(ARG_SECTION_NUMBER));
         rootView = inflater.inflate(R.layout.history_pages, container, false);
         workoutNameTextView = (TextView) rootView.findViewById(R.id.history_workout_title);
         workoutNameTextView.setText(workoutItem.getName());
-        //workoutHistoryView = (ListView) rootView.findViewById(R.id.history_workout_list_view);
-        //workoutHistoryView.
+        workoutHistoryView = (ListView) rootView.findViewById(R.id.history_workout_list_view);
+
+        workoutHistoryView.setAdapter(historyWorkoutAdapter);
 
 
         return rootView;
