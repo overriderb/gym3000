26208707d06df0618ee21e0827a362edd76d8154
overriderb <overriderb@gmail.com>
2015-08-02 14:00:07 +0300

    Added user infrastructure

---
+++ src/main/java/org/gym/model/User.java
@@ -0,0 +1,26 @@
+package org.gym.model;
+
+/**
+ * TODO: Add comment
+ */
+public class User {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
