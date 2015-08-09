9420a84e77e0ea39817bcb0a65b4a07dc50b39cc
overriderb <overriderb@gmail.com>
2015-08-09 12:45:04 +0300

    Changed initialize procedure

--- src/main/java/org/gym/Gym3000.java
+++ src/main/java/org/gym/Gym3000.java
@@ -19,11 +19,9 @@ public class Gym3000 extends Application {
     public void onCreate() {
         super.onCreate();
         DatabaseHelper.init(this);
-        if(isFirstStart()){
+        if(false){                                //For first start of application please change change isFirstStart()
             InitializeUtils.initTestPrograms();
-//        if(false){                                //For first start of application please change change isFirstStart()
-//            setInitialPrograms();
-//            setInitialPreferences();              //to true for correct setting to DB default programs
+            setInitialPreferences();              //to true for correct setting to DB default programs
         }
     }
 
@@ -54,88 +52,6 @@ public class Gym3000 extends Application {
         }
         return firstStartKey;
     }
-
-    /**
-     * Method sets to DB programs and workouts, which user will see after starting this application first time
-     */
-    private void setInitialPrograms() {
-        UserEntity user = new UserEntity("Andrew");
-        Program handsChestProgram1 = new Program(user.getId(), "1. Руки/грудь", "Тренировка групп мышц груди и рук", 1);
-        databaseHelper.getProgramRepository().storeProgram(handsChestProgram1);
-
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Разминка", "Согревающие/разминающие упражнения:\n" +
-                "-Растяжка/разминка\n" +
-                "-Бег/велотренажер", R.drawable.warm_up, 1));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Жим лежа, 90", "Жим лежа на лавке под углом 90", R.drawable.lateral_raise, 2));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Разводы гантелей сидя, 45", "Развод гантелей сидя на лавке под углом 45", R.drawable.lat_pulldown, 3));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Брусья","Отжимания от брусьев, можно с отягощением", R.drawable.cable_rows, 4));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Французский жим", "Французский жим лежа на лавке", R.drawable.cable_rows, 5));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Кроссовер, вниз, металл. ручка", "Трицепс на кроссовере с металлической ручкой", R.drawable.lat_pulldown, 6));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Молотки","Поочередное поднимание гантелей к плечам на бицепс", R.drawable.cable_rows, 7));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Штанга обратным хватом", "Поднимание штанги к плечам на бицепс обратным хватом", R.drawable.cable_rows, 8));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram1, "Пресс", "Пресс на римском стуле", R.drawable.lat_pulldown, 9));
-
-        Program legsBackProgram1 = new Program(user.getId(), "2. Ноги/спина", "Тренировка групп мышц спины и ног", 2);
-        databaseHelper.getProgramRepository().storeProgram(legsBackProgram1);
-
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Разминка", "Согревающие/разминающие упражнения:\n" +
-                "-Растяжка/разминка\n" +
-                "-Бег/велотренажер\n" +
-                "-Гиперэкстензия\n" +
-                "-Разминка ног на тренажерах", R.drawable.warm_up, 1));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Приседания",
-                "Приседания со штангой", R.drawable.crunch, 2));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Армейский жим",
-                "Поднимания штанги стоя/сидя над головой с груди. При выполнении смотреть строго перед собой, " +
-                        "руки шире плеч", R.drawable.back_extensions, 3));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Подьем гантелей",
-                "Попеременный подьем гантелей перед собой на вытянутых руках. Подымать по широкой дуге, " +
-                        "одна рука опускается, другая - поднимается, расходясь у лица", R.drawable.cardio_trainings, 4));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Тяга к подбородку",
-                "Тяга штанги к подбородку узким хватом", R.drawable.crunch, 5));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Спина",
-                "Упражнения на спину, которые я пока не придумал", R.drawable.crunch, 6));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram1, "Пресс",
-                "Пресс на сжигание", R.drawable.crunch, 7));
-
-        Program handsChestProgram2 = new Program(user.getId(), "3. Руки/грудь", "Тренировка групп мышц груди и рук", 3);
-        databaseHelper.getProgramRepository().storeProgram(handsChestProgram2);
-
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Разминка", "Согревающие/разминающие упражнения:\n" +
-                "-Растяжка/разминка\n" +
-                "-Бег/велотренажер", R.drawable.warm_up, 1));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Жим лежа, обратный угол", "Жим лежа на лавке под обратным углом", R.drawable.lateral_raise, 2));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Бабочка", "Разведение вытянутых рукна тренажере", R.drawable.lat_pulldown, 3));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Жим узким хватом, 90","Жим лежа на лавке под 90, узкий хват", R.drawable.cable_rows, 4));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Кроссовер, вверх, веревка", "Трицепс на кроссовере с веревкой. Тяга троса из-за голвы вверх", R.drawable.cable_rows, 5));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Кроссовер, вниз, веревка", "Трицепс на кроссовере с веревкой", R.drawable.lat_pulldown, 6));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Штанга прямым хватом","Поднимание штанги к плечам на бицепс прямым хватом", R.drawable.cable_rows, 7));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Гантель через колено", "Поднимание гантели к плечу через колено, выполняется сидя", R.drawable.cable_rows, 8));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(handsChestProgram2, "Пресс", "Пресс на римском стуле", R.drawable.lat_pulldown, 9));
-
-        Program legsBackProgram2 = new Program(user.getId(), "4. Ноги/спина", "Тренировка групп мышц спины и ног", 4);
-        databaseHelper.getProgramRepository().storeProgram(legsBackProgram2);
-
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Разминка", "Согревающие/разминающие упражнения:\n" +
-                "-Растяжка/разминка\n" +
-                "-Бег/велотренажер\n" +
-                "-Гиперэкстензия\n" +
-                "-Разминка ног на тренажерах", R.drawable.warm_up, 1));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Становая тяга",
-                "Тяга штанги", R.drawable.crunch, 2));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Жим штанги из-за головы",
-                "Выполняется стоя/сидя, голова прямо, не наклонять ее вперед, слегка прогнуться в пояснице", R.drawable.back_extensions, 3));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Разведение гантелей",
-                "Одновременное разведение гантелей стоя. Выполнение: не раскачиваться, в верхней точке разварачивать " +
-                        "кисти рук внутрь, чтобы задняя часть гантели была выше передней", R.drawable.cardio_trainings, 4));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Шраги",
-                "Шраги гантелями/штангой", R.drawable.crunch, 5));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Спина",
-                "Упражнения на спину, которые я пока не придумал", R.drawable.crunch, 6));
-        databaseHelper.getWorkoutRepository().storeWorkout(new Workout(legsBackProgram2, "Пресс",
-                "Пресс на сжигание", R.drawable.crunch, 7));
-    }
-
 
     private void setInitialPreferences() {
         SharedPreferencesHelper.setBool(this, SharedPreferencesHelper.IS_PICTURE_OPEN, true);
--- src/main/java/org/gym/repository/InitializeUtils.java
+++ src/main/java/org/gym/repository/InitializeUtils.java
@@ -1,130 +1,122 @@
 package org.gym.repository;
 
 import org.gym.activity.R;
-import org.gym.domain.AttemptEntity;
-import org.gym.domain.ExerciseEntity;
 import org.gym.domain.ExerciseTypeEntity;
 import org.gym.domain.ProgramEntity;
 import org.gym.domain.UserEntity;
-import org.gym.domain.WorkoutEntity;
-
-import java.util.Calendar;
 
 /**
  * TODO: Add comment
  */
 public class InitializeUtils {
 
+    private static UserRepository userRepository = UserRepository.getInstance();
+    private static ProgramRepository programRepository = ProgramRepository.getInstance();
+    private static ExerciseTypeRepository exerciseTypeRepository = ExerciseTypeRepository.getInstance();
+
     /**
      * Method sets to DB programs and workouts, which user will see after starting this application first time
      */
     public static void initTestPrograms() {
-        UserRepository userRepository = UserRepository.getInstance();
-        ProgramRepository programRepository = ProgramRepository.getInstance();
-        ExerciseTypeRepository exerciseTypeRepository = ExerciseTypeRepository.getInstance();
-        WorkoutRepository workoutRepository = WorkoutRepository.getInstance();
-        AttemptRepository attemptRepository = AttemptRepository.getInstance();
-        ExerciseRepository exerciseRepository = ExerciseRepository.getInstance();
 
         UserEntity user = new UserEntity("Andrew");
         Long userStoredId = userRepository.store(user);
         user.setId(userStoredId);
 
+        initFirstHandsChestProgram();
+        initFirstLegsBackProgram();
+        initSecondHandsChestProgram();
+        initSecondLegsBackProgram();
+    }
+
+    // #1 First hands and chest program
+    private static void initFirstHandsChestProgram() {
         ProgramEntity handsChestFirstProgramEntity = new ProgramEntity("1. Руки/грудь", "Тренировка групп мышц груди и рук", 1);
-        Long handsChestProgramStoredId = programRepository.store(handsChestFirstProgramEntity);
-        handsChestFirstProgramEntity.setId(handsChestProgramStoredId);
+        Long handsChestFirstProgramStoredId = programRepository.store(handsChestFirstProgramEntity);
+        handsChestFirstProgramEntity.setId(handsChestFirstProgramStoredId);
 
-        ProgramEntity legsBackProgramEntity = new ProgramEntity("2. Ноги/спина", "Тренировка групп мышц спины и ног", 2);
-        Long legsBackProgramStoredId = programRepository.store(legsBackProgramEntity);
-        legsBackProgramEntity.setId(legsBackProgramStoredId);
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Разминка", "Согревающие/разминающие упражнения:\n" +
+                "-Растяжка/разминка\n" +
+                "-Бег/велотренажер", R.drawable.warm_up, 1));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Жим лежа, 90", "Жим лежа на лавке под углом 90", R.drawable.lateral_raise, 1));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Разводы гантелей сидя, 45", "Развод гантелей сидя на лавке под углом 45", R.drawable.lat_pulldown, 3));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Брусья","Отжимания от брусьев, можно с отягощением", R.drawable.cable_rows, 4));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Французский жим", "Французский жим лежа на лавке", R.drawable.cable_rows, 5));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Кроссовер, вниз, металл. ручка", "Трицепс на кроссовере с металлической ручкой", R.drawable.lat_pulldown, 6));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Молотки","Поочередное поднимание гантелей к плечам на бицепс", R.drawable.cable_rows, 7));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Штанга обратным хватом", "Поднимание штанги к плечам на бицепс обратным хватом", R.drawable.cable_rows, 8));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestFirstProgramStoredId, "Пресс", "Пресс на римском стуле", R.drawable.lat_pulldown, 9));
+    }
 
+    // #2 First legs and back program
+    private static void initFirstLegsBackProgram() {
+        ProgramEntity legsBackFirstProgramEntity = new ProgramEntity("2. Ноги/спина", "Тренировка групп мышц спины и ног", 2);
+        Long legsBackFirstProgramStoredId = programRepository.store(legsBackFirstProgramEntity);
+        legsBackFirstProgramEntity.setId(legsBackFirstProgramStoredId);
+
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Разминка", "Согревающие/разминающие упражнения:\n" +
+                "-Растяжка/разминка\n" +
+                "-Бег/велотренажер\n" +
+                "-Гиперэкстензия\n" +
+                "-Разминка ног на тренажерах", R.drawable.warm_up, 1));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Приседания",
+                "Приседания со штангой", R.drawable.crunch, 2));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Армейский жим",
+                "Поднимания штанги стоя/сидя над головой с груди. При выполнении смотреть строго перед собой, " +
+                        "руки шире плеч", R.drawable.back_extensions, 3));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Подьем гантелей",
+                "Попеременный подьем гантелей перед собой на вытянутых руках. Подымать по широкой дуге, " +
+                        "одна рука опускается, другая - поднимается, расходясь у лица", R.drawable.cardio_trainings, 4));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Тяга к подбородку",
+                "Тяга штанги к подбородку узким хватом", R.drawable.crunch, 5));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Спина",
+                "Упражнения на спину, которые я пока не придумал", R.drawable.crunch, 6));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackFirstProgramStoredId, "Пресс",
+                "Пресс на сжигание", R.drawable.crunch, 7));
+    }
+
+    // #3 Second hands and chest program
+    private static void initSecondHandsChestProgram() {
         ProgramEntity handsChestSecondProgramEntity = new ProgramEntity("3. Руки/грудь", "Тренировка групп мышц груди и рук", 3);
         Long handsChestSecondProgramStoredId = programRepository.store(handsChestSecondProgramEntity);
         handsChestSecondProgramEntity.setId(handsChestSecondProgramStoredId);
 
-        ExerciseTypeEntity warmUpExerciseTypeEntity = new ExerciseTypeEntity(handsChestProgramStoredId, "Warm-up", "Some warm-up exercises. It is important to warming-up and " +
-                "stretch every muscle which will be burden.", R.drawable.warm_up);
-        Long warmUpExerciseTypeStoredId = exerciseTypeRepository.store(warmUpExerciseTypeEntity);
-        warmUpExerciseTypeEntity.setId(warmUpExerciseTypeStoredId);
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Разминка", "Согревающие/разминающие упражнения:\n" +
+                "-Растяжка/разминка\n" +
+                "-Бег/велотренажер", R.drawable.warm_up, 1));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Жим лежа, обратный угол", "Жим лежа на лавке под обратным углом", R.drawable.lateral_raise, 2));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Бабочка", "Разведение вытянутых рукна тренажере", R.drawable.lat_pulldown, 3));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Жим узким хватом, 90","Жим лежа на лавке под 90, узкий хват", R.drawable.cable_rows, 4));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Кроссовер, вверх, веревка", "Трицепс на кроссовере с веревкой. Тяга троса из-за голвы вверх", R.drawable.cable_rows, 5));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Кроссовер, вниз, веревка", "Трицепс на кроссовере с веревкой", R.drawable.lat_pulldown, 6));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Штанга прямым хватом","Поднимание штанги к плечам на бицепс прямым хватом", R.drawable.cable_rows, 7));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Гантель через колено", "Поднимание гантели к плечу через колено, выполняется сидя", R.drawable.cable_rows, 8));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(handsChestSecondProgramStoredId, "Пресс", "Пресс на римском стуле", R.drawable.lat_pulldown, 9));
+    }
 
-        ExerciseTypeEntity lateralRaiseExerciseTypeEntity = new ExerciseTypeEntity(handsChestProgramStoredId, "Lateral raise", "The lateral raise with dumbbells is an effective " +
-                "exercise for developing the deltoids, and is performed by extending the arm to the side of the body " +
-                "with the elbow extended.\n lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text \n" +
-                " lot of text", R.drawable.lateral_raise);
-        Long lateralRaiseExerciseTypeStoredId = exerciseTypeRepository.store(lateralRaiseExerciseTypeEntity);
-        lateralRaiseExerciseTypeEntity.setId(lateralRaiseExerciseTypeStoredId);
+    // #4 Second legs and back program
+    private static void initSecondLegsBackProgram() {
+        ProgramEntity legsBackSecondProgramEntity = new ProgramEntity("4. Ноги/спина", "Тренировка групп мышц спины и ног", 4);
+        Long legsBackSecondProgramStoredId = programRepository.store(legsBackSecondProgramEntity);
+        legsBackSecondProgramEntity.setId(legsBackSecondProgramStoredId);
 
-        ExerciseTypeEntity latPulldownExerciseTypeEntity = new ExerciseTypeEntity(handsChestProgramStoredId, "Lat pulldown", "The lat pulldown works the major muscles in the back and " +
-                "also the biceps in the arms. It's a good exercise to progress up to doing pull ups as it develops " +
-                "strength in the back and arms, so its ideal for beginners.", R.drawable.lat_pulldown);
-        Long latPulldownExerciseTypeStoredId =exerciseTypeRepository.store(latPulldownExerciseTypeEntity);
-        latPulldownExerciseTypeEntity.setId(latPulldownExerciseTypeStoredId);
-
-        ExerciseTypeEntity cableRowsExerciseTypeEntity = new ExerciseTypeEntity(handsChestProgramStoredId, "Cable rows", "Seated cable rows are a great way to work the back muscles. " +
-                "Varying the width of your hands focuses the intensity on different areas of the back. Because it's a " +
-                "pulling exercise you are also working your biceps.", R.drawable.cable_rows);
-        Long cableRowsExerciseTypeStoredId = exerciseTypeRepository.store(cableRowsExerciseTypeEntity);
-        cableRowsExerciseTypeEntity.setId(cableRowsExerciseTypeStoredId);
-
-        ExerciseTypeEntity tricepPushdownsExerciseTypeEntity = new ExerciseTypeEntity(legsBackProgramStoredId, "Tricep pushdowns", "Tricep pushdowns isolate the tricep muscles and are " +
-                "usually done at the tail end of a workout to give the triceps a real pump. It's a very easy exercise " +
-                "to perform and is ideal for beginners to weight training.", R.drawable.tricep_pushdowns);
-        Long tricepPushdownsExerciseTypeStoredId =exerciseTypeRepository.store(tricepPushdownsExerciseTypeEntity);
-        tricepPushdownsExerciseTypeEntity.setId(tricepPushdownsExerciseTypeStoredId);
-
-        ExerciseTypeEntity crunchExerciseTypeEntity = new ExerciseTypeEntity(legsBackProgramStoredId, "Crunch", " The standard abdominal crunch exercise targets the stomach " +
-                "muscles. The crunch is a safe and effective exercise that is great for beginners to help develop " +
-                "strong abdominal muscles.", R.drawable.crunch);
-        Long crunchExerciseTypeStoredId = exerciseTypeRepository.store(crunchExerciseTypeEntity);
-        crunchExerciseTypeEntity.setId(crunchExerciseTypeStoredId);
-
-        ExerciseTypeEntity backExtensionsExerciseTypeEntity = new ExerciseTypeEntity(legsBackProgramStoredId, "Back extensions", "Back extensions are also known as hyperextensions. It's " +
-                "an exercise that works the lower back and also the buttocks and hamstrings.", R.drawable.back_extensions);
-        Long backExtensionsExerciseTypeStoredId = exerciseTypeRepository.store(backExtensionsExerciseTypeEntity);
-        backExtensionsExerciseTypeEntity.setId(backExtensionsExerciseTypeStoredId);
-
-        ExerciseTypeEntity cardioTrainingsExerciseTypeEntity = new ExerciseTypeEntity(legsBackProgramStoredId, "Cardio trainings", "Training for pussy-man, who doesn't want to kick " +
-                "somebody's ass on street, only likes pedalling", R.drawable.cardio_trainings);
-        Long cardioTrainingsExerciseTypeStoredId = exerciseTypeRepository.store(cardioTrainingsExerciseTypeEntity);
-        cardioTrainingsExerciseTypeEntity.setId(cardioTrainingsExerciseTypeStoredId);
-
-        WorkoutEntity workoutEntity = new WorkoutEntity(handsChestProgramStoredId, Calendar.getInstance().getTimeInMillis(), Calendar.getInstance().getTimeInMillis(), "FINISHED");
-        Long storedFirstWorkout = workoutRepository.store(workoutEntity);
-        workoutEntity.setId(storedFirstWorkout);
-
-        ExerciseEntity firstExerciseEntity = new ExerciseEntity("M", storedFirstWorkout, lateralRaiseExerciseTypeStoredId);
-        Long storedFistExerciseId = exerciseRepository.store(firstExerciseEntity);
-        firstExerciseEntity.setId(storedFistExerciseId);
-        attemptRepository.store(new AttemptEntity(storedFistExerciseId, "10", 15));
-        attemptRepository.store(new AttemptEntity(storedFistExerciseId, "15", 13));
-
-        ExerciseEntity secondExerciseEntity = new ExerciseEntity("L",storedFirstWorkout, cableRowsExerciseTypeStoredId);
-        Long storedSecondExerciseId = exerciseRepository.store(secondExerciseEntity);
-        secondExerciseEntity.setId(storedSecondExerciseId);
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "8", 20));
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "10", 15));
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "15", 15));
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "20", 8));
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "25", 15));
-        attemptRepository.store(new AttemptEntity(storedSecondExerciseId, "115", 4));
-
-        ExerciseEntity thirdExerciseEntity = new ExerciseEntity("S", storedFirstWorkout, backExtensionsExerciseTypeStoredId);
-        Long storedThirdExerciseId = exerciseRepository.store(thirdExerciseEntity);
-        thirdExerciseEntity.setId(storedThirdExerciseId);
-        attemptRepository.store(new AttemptEntity(storedThirdExerciseId, "10", 15));
-        attemptRepository.store(new AttemptEntity(storedThirdExerciseId, "15", 13));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Разминка", "Согревающие/разминающие упражнения:\n" +
+                "-Растяжка/разминка\n" +
+                "-Бег/велотренажер\n" +
+                "-Гиперэкстензия\n" +
+                "-Разминка ног на тренажерах", R.drawable.warm_up, 1));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Становая тяга",
+                "Тяга штанги", R.drawable.crunch, 2));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Жим штанги из-за головы",
+                "Выполняется стоя/сидя, голова прямо, не наклонять ее вперед, слегка прогнуться в пояснице", R.drawable.back_extensions, 3));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Разведение гантелей",
+                "Одновременное разведение гантелей стоя. Выполнение: не раскачиваться, в верхней точке разварачивать " +
+                        "кисти рук внутрь, чтобы задняя часть гантели была выше передней", R.drawable.cardio_trainings, 4));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Шраги",
+                "Шраги гантелями/штангой", R.drawable.crunch, 5));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Спина",
+                "Упражнения на спину, которые я пока не придумал", R.drawable.crunch, 6));
+        exerciseTypeRepository.store(new ExerciseTypeEntity(legsBackSecondProgramStoredId, "Пресс",
+                "Пресс на сжигание", R.drawable.crunch, 7));
     }
 }
